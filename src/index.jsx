34992fb339139b885c4aef1e9a3465aa62d2ccd8
Malithi Karunarathne <mkarunarathne997@gmail.com>
2023-12-16 07:51:53 +0530

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+
+import App from './App.jsx';
+import './index.css';
+
+<header>
+    <h1>React Investment Calculator</h1>
+</header>
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
